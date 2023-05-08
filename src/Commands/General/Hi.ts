737b4483062260cf82d117316b5e7e9a8e2eaf7e
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-05-08 18:56:35 +0530

    Update Hi.ts

--- src/Commands/General/Hi.ts
+++ src/Commands/General/Hi.ts
@@ -10,5 +10,5 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class extends BaseCommand {
     public override execute = async ({ sender, reply }: Message): Promise<void> =>
-        void (await reply(`Hello! *${sender.username}*`))
+    void (await reply(`Hello! *${sender.username}, this is a bot developed by Ayush wa.me/16468338933. It will be launched for y'all soon*`))
 }
