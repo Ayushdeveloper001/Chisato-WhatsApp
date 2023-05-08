d1c40e0abb5acdae49f39c1282b20ac1ff5e0997
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-05-08 17:45:47 +0530

    Update Info.ts

--- src/Commands/General/Info.ts
+++ src/Commands/General/Info.ts
@@ -17,7 +17,7 @@ export default class extends BaseCommand {
         }
         const image = this.client.assets.get('chisato') as Buffer
         const uptime = this.client.utils.formatSeconds(process.uptime())
-        const text = `*Chisato-WhatsApp* \n\n📡 *Description: ${description}*\n\n🛠️ *Commands:* ${this.handler.commands.size}\n\n🛡️ *Uptime:* ${uptime}`
+        const text = `*Ayush-MD* \n\n📡 *Description: ${description}*\n\n🛠️ *Commands:* ${this.handler.commands.size}\n\n🛡️ *Uptime:* ${uptime}`
         return void (await reply(image, 'image', undefined, undefined, text, undefined, {
             title: this.client.utils.capitalize(name),
             thumbnail: image,
